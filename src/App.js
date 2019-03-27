1d1fdc762125e1e3b1fe57e68141039813605280
Nicolas Castro <nicolascastro@me.com>
2019-03-27 19:58:11 -0400

    Added Routes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Home from './Home';
+import About from './About';
+import Navbar from './Navbar';
+import Education from './Education'
+import Experience from './Experience'
+import { Switch, Route } from 'react-router-dom';
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Navbar />
+
+        <Switch>
+          <Route exact path='/' component={Home}/>
+          <Route path='/about' component={About}/>
+          <Route path='/education' component={Education}/>
+          <Route path='/experience' component={Experience}/>
+        </Switch>
       </div>
     );
   }
